be6ab1a27d2033b793c841cf3c078ca1b88eda82
EC2 Default User <ec2-user@ip-172-31-35-134.ec2.internal>
2022-02-08 03:31:58 +0000

    [Finish]4-5

--- meshiterro/config/routes.rb
+++ meshiterro/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # deviseモデルを作成したことで下記のルーティンングが自動追加
+  # deviseを使用する際にURLとしてusersを含むことを
+  devise_for :users
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
