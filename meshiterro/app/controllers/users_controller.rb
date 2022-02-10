bb31506829b648c2945a6ebd8c3e167773c24a10
EC2 Default User <ec2-user@ip-172-31-35-134.ec2.internal>
2022-02-10 13:17:37 +0000

    [Finish]4-15

--- meshiterro/app/controllers/users_controller.rb
+++ meshiterro/app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    # アソシエーションを持っているモデル同士の記述方法↓
+    # 特定のユーザー(@user)に関連づけられた投稿全て(.post_images)を取得し、@post_images
+    @post_images = @user.post_images
   end
 
   def edit
